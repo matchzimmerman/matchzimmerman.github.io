a5ad7d03e2316c022b75b2309f764651b89dd834
matchzimmerman <matchzimmerman@gmail.com>
2026-09-24 22:57:11 -0400

    Cache qualified Printful knit product manifest

---
+++ shop-api/lib/knit-products.js
@@ -0,0 +1,10 @@
+module.exports = [
+  { id: 781, name: "Unisex Knitted Cardigan" },
+  { id: 769, name: "Unisex Classic Fit Knitted Crew neck Sweater" },
+  { id: 964, name: "Knitted Pet Sweater" },
+  { id: 860, name: "Unisex Relaxed Fit Knitted Crew Neck Sweater" },
+  { id: 859, name: "Women's Straight Bottom Knitted Sweater" },
+  { id: 858, name: "Unisex Knitted V-Neck Vest" },
+  { id: 855, name: "Unisex Knitted T-shirt" },
+  { id: 850, name: "Knitted Beanie" }
+];
